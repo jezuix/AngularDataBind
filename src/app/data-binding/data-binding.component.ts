32e60791dc47c014dba3350634a186a7cb6c9b40
mrcondor <mr_condor_@hotmail.com>
2019-09-16 23:41:32 -0300

    Eventos

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -10,14 +10,39 @@ export class DataBindingComponent implements OnInit {
   url: string = 'http:loiane.com';
   cursoAngular: boolean = true;
   urlImagem: string = 'http://lorempixel.com/400/200/';
+  valorAtual: string = '';
+  valorSalvo: string = '';
 
-  
+  isMouseOver: boolean = false;
+
+  nome: string = 'abc';
+  pessoa: any = {
+    nome: 'def',
+    idade: 30
+  }
+
   getValor(){
     return 1;
   }
 
   getCurtirCurso(){
     return true;
+  }
+
+  botaoClickado(){
+    alert("Botão clickado");
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor: string){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
